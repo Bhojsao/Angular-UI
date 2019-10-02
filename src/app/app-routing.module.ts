53d626d75dc206b567583b36766ccc00121723a1
Bhoj Sao <bhojsao@ymail.com>
2019-10-02 15:56:26 +0530

    Updated AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {RouterModule, Routes} from '@angular/router';
 import { ProductDetailsComponent } from './product-management/product-details/product-details.component';
 import {LoginComponent} from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
 import { ProductModule } from './product-management/product.module';
 
 export function getDefaultRoute(mode :any) : string{
@@ -15,14 +14,13 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'product', component: ProductDetailsComponent },
-  { path: 'test', component: TestComponent }
 ];
 
 @NgModule({
   declarations: [
     LoginComponent,
-    HomeComponent,
-    TestComponent],
+    HomeComponent
+  ],
   imports: [
     RouterModule.forRoot(appRoutes,{onSameUrlNavigation:'reload'}),
     CommonModule,
